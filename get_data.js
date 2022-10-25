f9af721a9067df401d9501bedd71bf16c39bf4d3
For Fun <dnce17@Dannys-MacBook-Air.local>
2022-10-25 00:37:54 -0400

    placing project into repo

---
+++ get_data.js
@@ -0,0 +1,19 @@
+let type = document.querySelector("#type");
+let evolMethod = document.querySelector("#evol-method");
+
+function getData(item) {
+    // console.log(item.value); // returns e.g. STRING (emphasis on this) "pikachu"
+    // type.value = item.type;
+    // evolMethod.value = item.howToEvolve;
+
+    for (key in pokemon) {
+        // console.log(pokemon[key].name); 
+        if (pokemon[key].name == item.value) {
+            console.log("Yes, it is the same.");
+            type.value = pokemon[key].type;
+            evolMethod.value = pokemon[key].howToEvolve;
+            // console.log(pokemon[key].type);
+            // console.log(pokemon[key].howToEvolve);
+        }
+    }
+}
---
+++ module1.js
@@ -0,0 +1,35 @@
+// const pikachu = {
+//     name: "Pikachu",
+//     type: "Electric",
+//     howToEvolve: "Thunderstone"
+// }
+
+// const bulbasaur = {
+//     type: "Grass/Poison",
+//     howToEvolve: "Level 16"
+// }
+
+let pokemon = {
+
+    pikachu: {
+        name: "Pikachu",
+        type: "Electric",
+        howToEvolve: "Thunderstone"
+    },
+
+    bulbasaur: {
+        name: "Bulbasaur",
+        type: "Grass/Poison",
+        howToEvolve: "Level 16"
+    }
+}
+
+// let test = "Bulbasaur"
+
+// for (key in pokemon) {
+//     if (pokemon[key].name == test) {
+//         // console.log("yay")
+//         console.log(pokemon[key].type);
+//         console.log(pokemon[key].howToEvolve);
+//     }
+// }
